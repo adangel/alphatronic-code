21d182a508bed6c4d51f7ae3cb472037db1490d4
Andreas Dangel <andreas.dangel@adangel.org>
2023-07-09 11:33:17 +0200

    Add JavaSerialClock2

---
+++ JavaSerialClock/src/main/java/org/adangel/alphatronic/serialclock/JavaSerialClock2.java
@@ -0,0 +1,33 @@
+package org.adangel.alphatronic.serialclock;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class JavaSerialClock2 {
+    public static void main(String[] args) throws InterruptedException {
+        SerialPort commPort = SerialPort.getCommPort("/dev/ttyUSB0");
+        System.out.println("commPort = " + commPort + " (" + commPort.getSystemPortPath() + ")");
+
+        int baudrate = 2400;
+        // int baudrate = 100*2;
+        boolean set = commPort.setBaudRate(baudrate);
+        System.out.println("set = " + set);
+        commPort.setParity(SerialPort.NO_PARITY);
+        commPort.setNumDataBits(8);
+        commPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        commPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        commPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, (int) TimeUnit.SECONDS.toMillis(30), 0);
+        commPort.openPort();
+
+        System.out.println("Starting clock with " + (baudrate / 2) + "Hz");
+        byte[] buffer = new byte[20];
+        Arrays.fill(buffer, (byte) 0x55);
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis() - start < TimeUnit.SECONDS.toMillis(10)) {
+            commPort.writeBytes(buffer, buffer.length);
+        }
+        System.out.println("Clock done");
+    }
+}
